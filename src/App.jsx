3b1626f4006bcb0747739fefd14c3144e98840da
kaustubh2020 <kaustubhjaiswal200@gamil.com>
2023-11-20 11:41:44 +0530

    Mapping projects on sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -21,19 +21,19 @@ function App() {
 
   function handleAddProject(projectData) {
     setProjectsState((prevState) => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        id: projectId,
       };
 
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
   }
-
-  console.log(projectsState);
 
   let content;
   if (projectsState.selectedProjectId === null) {
@@ -44,7 +44,10 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSideBar onStartAddProject={handleStartAddProject} />
+      <ProjectsSideBar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
